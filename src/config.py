584f4b500b99134a02e0de4aa18fb201bd5882e3
Erin <moorhead.erinm@gmail.com>
2026-09-17 11:25:49 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -64,7 +64,7 @@ Please contact Erin [:mailbox:](https://www.nipissingu.ca/users/erin-moorhead) t
 HELP_MESSAGE = """
 
 
-_Need more help:question: Contact Erin [:mailbox:](https://www.nipissingu.ca/users/erin-moorhead)
+Need more help:question: Contact Erin [:mailbox:](https://www.nipissingu.ca/users/erin-moorhead)
 """
 
 ### Other Configs
